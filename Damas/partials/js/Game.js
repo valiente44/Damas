a06eafb7ad63bdb28fcc3e8510717ea2b5027acb
Your Name <you@example.com>
2021-10-21 20:40:51 +0200

    Update dynamic checkers

--- Damas/partials/js/Game.js
+++ Damas/partials/js/Game.js
@@ -5,25 +5,33 @@ class Game  {
         this.start = true;
         this.whitePuntuation = 12;
         this.blackPuntuation = 12;
+        this.white_cheekers = [];
+        this.black_cheekers = [];
         //If turn == true, white
         //If turn == false, black
         this.turn = true;
+
+        //Initialize view
+        $('#player1_name').html(this.player1);
+        $('#player2_name').html(this.player2);
+        $('#blackPuntuation').html(this.blackPuntuation);
+        $('#whitePuntuation').html(this.whitePuntuation);
     }
      
     initializeBoard() {
         var board = '';
         var swap = true;
-        for(var i = 0; i < 8; i++) {
-            for(var f = 0; f < 8; f++) {
+        for(var fil = 0; fil < 8; fil++) {
+            for(var col = 0; col < 8; col++) {
                 if(swap) {
-                    board += '<div class="square white_square"></div>';
+                    board += '<div class="square white_square" id="' + fil + '-' + col + '"></div>';
                 } else {
-                    if(i >= 0 && i < 3) {
-                        board += '<div class="square black_square"><div class="checker black_checker"></div></div>';
-                    } else if(i >= 5 && i < 9) {
-                        board += '<div class="square black_square"><div class="checker white_checker"></div></div>';
+                    if(fil >= 0 && fil < 3) {
+                        board += '<div class="square black_square" id="' + fil + '-' + col + '"><div class="checker black_checker"></div></div>';
+                    } else if(fil >= 5 && fil < 9) {
+                        board += '<div class="square black_square" id="' + fil + '-' + col + '"><div class="checker white_checker"></div></div>';
                     } else {
-                        board += '<div class="square black_square"></div>';
+                        board += '<div class="square black_square" id="' + fil + '-' + col + '"></div>';
                     }
                 }
                 swap = !swap;
@@ -31,9 +39,38 @@ class Game  {
             swap = !swap;
         }
     
-        this.white_cheekers = document.getElementsByClassName("white_checker");
-        this.black_cheekers = document.getElementsByClassName("black_cheekers");
-    
         $('#table').html(board);
+
+        var arrayWhiteCheckers = this.white_cheekers;
+        var arrayBlackCheckers = this.black_cheekers;
+
+        var id = 1;
+        var outside = this;
+
+        $(".white_checker").each(function(e) {
+            var checker = new Checker((id + '-w'), '#FFFFFF', $(this).parent().attr('id'));
+            arrayWhiteCheckers.push(checker);
+            id++;
+
+            $(this).on('click', function(e) {
+                outside.showAvailablePositions(true, checker.position);
+            });
+        });
+
+        $(".black_checker").each(function(e) {
+            var checker = new Checker((id + '-b'), '#000000', $(this).parent().attr('id'));
+            arrayBlackCheckers.push(checker);
+            id++;
+
+            $(this).on('click', function(e) {
+                outside.showAvailablePositions(false, checker.position);
+            });
+        });
+    }
+
+    showAvailablePositions(isWhite, position) {
+        if(this.turn == isWhite) {
+            alert('Is white? ' + isWhite + ', Position: ' + position);
+        }
     }
 }
